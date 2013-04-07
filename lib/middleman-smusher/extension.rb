b98427b629ddccd2e96cf979c7c8aaaa3ece4809
the-architect <marcel.scherf@epicteams.com>
2013-04-07 04:15:10 +0200

    drop the queue, just synchronize the files array instead

--- lib/middleman-smusher/extension.rb
+++ lib/middleman-smusher/extension.rb
@@ -6,14 +6,14 @@ module Middleman
     class << self
       def registered(app, options={})
         require 'smusher'
-
         # options[:service] ||= "SmushIt"
         options[:quiet] = true
 
         app.after_configuration do
-          smush_dir    = options.fetch(:path, File.join(build_dir, images_dir))
-          thread_count = options.fetch(:threads, DEFAULT_THREADS)
-          prefix       = build_dir + File::SEPARATOR
+          smush_dir     = options.fetch(:path, File.join(build_dir, images_dir))
+          thread_count  = options.fetch(:threads, DEFAULT_THREADS)
+          in_parallel   = options.fetch(:parallel, true)
+          prefix        = build_dir + File::SEPARATOR
         
           after_build do |builder|
 
@@ -23,28 +23,36 @@ module Middleman
 
             builder.say_status :smushing, "%d files. This may take a moment." % files.size
 
-            queue = Queue.new
-            files.each{|f| queue << f }
+            if in_parallel
+              # parallel smushing
+              mutex = Mutex.new
+              threads = []
 
-            mutex = Mutex.new
-            threads = []
+              thread_count.times do
+                threads << Thread.new do
+                  loop do
+                    file = nil
+                    mutex.synchronize do
+                      file = files.pop
+                    end
+                    break unless file
 
-            thread_count.times do
-              threads << Thread.new do
-                loop do
-                  break if queue.empty?
-                  file = queue.pop(true)
-                  break unless file
+                    ::Smusher.optimize_image [file], options
 
-                  ::Smusher.optimize_image [file], options
-
-                  mutex.synchronize{
-                    builder.say_status :smushed, file.sub(prefix, '')
-                  }
+                    mutex.synchronize do
+                      builder.say_status :smushed, file.sub(prefix, '')
+                    end
+                  end
                 end
               end
+              threads.each(&:join)
+            else
+              # serial smushing
+              files.each do |file|
+                ::Smusher.optimize_image [file], options
+                builder.say_status :smushed, file.sub(prefix, '')
+              end
             end
-            threads.each(&:join)
 
           end
         end
